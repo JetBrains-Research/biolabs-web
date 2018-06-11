35cc7a3d58d481618e227a4c7d2d531d5463765c
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2018-06-11 18:38:17 +0300

    Updated download, added download public data

--- site/generate.py
+++ site/generate.py
@@ -4,59 +4,24 @@ import shutil
 from pathlib import Path
 
 # Hardcoded URLs with data
-from sessions.aging_session import search_in_url, get_color
+from sessions.aging_session import search_in_url, get_color, HREF_MASK, HIST_TOOL_PATH_MAP, GSM_HIST_MAP, \
+    Y20O20_CONSENSUS_PATH, LABELS_URL, Y20O20_BW_PATH, Y20O20_BB_PATH, ENCODE_BW_PATH, ENCODE_BB_PATH
 
-Y20O20_CONSENSUS_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
-                        "/Y20O20/peaks/{}/zinbra/consensus"
-Y20O20_BW_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq/Y20O20" \
-                 "/bigwigs/{}"
-Y20O20_BB_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
-                 "/Y20O20/peaks/{}/{}/bigBed"
-ENCODE_BW_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
-                 "/cd14encode/bigwigs"
-ENCODE_BB_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
-                 "/cd14encode/peaks/{}/{}/bigBed"
-LABELS_URL = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq/Y20O20" \
-             "/labels/{}_labels.bed"
+ENCODE_PEAKS_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
+                    "/cd14encode/peaks/{}"
+ENCODE_LABELS_URL = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq/cd14encode" \
+                    "/labels/{}_labels.bed"
 
 BEDGZ_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
              "/Y20O20/bedgz/{}"
+FASTQC_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
+              "/Y20O20/qc/fastq/{}/fastqc/"
 PEAKS_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
              "/Y20O20/peaks/{}/{}"
 ZINBRA_MODELS_PATH = "https://artyomovlab.wustl.edu/publications/supp_materials/aging/chipseq" \
                      "/Y20O20/zinbra"
 
-GSM_HIST_MAP = {
-    'H3K27ac': 'GSM1102782',
-    'H3K27me3': 'GSM1102785',
-    'H3K36me3': 'GSM1102788',
-    'H3K4me1': 'GSM1102793',
-    'H3K4me3': 'GSM1102797'
-}
-HIST_TOOL_PATH_MAP = {
-    'H3K27ac': {'macs_broad'},
-    'H3K27me3': {'macs_broad', 'sicer'},
-    'H3K36me3': {'macs_broad', 'sicer'},
-    'H3K4me1': {'macs_broad'},
-    'H3K4me3': {'macs_broad'}
-}
-HIST_COLOR_MAP = {
-    "H3K27ac": (255, 0, 0),
-    "H3K27me3": (153, 0, 255),
-    "H3K4me1": (255, 153, 0),
-    "H3K4me3": (51, 204, 51),
-    "H3K36me3": (0, 0, 204)
-}
-TOOL_COLOR_MAP = {
-    "broad": (55, 126, 184),
-    "island": (228, 26, 28),
-    "peaks": (152, 78, 163),
-    "zinbra": (152, 78, 163)
-}
-
-HREF_MASK = '<a href="([^"]*.{})">'
-
-failed_tracks = []
+GSM_URL = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={}"
 
 FOLDER = os.path.dirname(__file__)
 OUT_FOLDER = FOLDER + '/out'
@@ -92,14 +57,14 @@ def generate_explore_data_page(hist, page):
         for i in range(0, len(y20o20_bws)):
             tracks.extend(format_tracks(hist, [y20o20_bws[i]],
                                         name_processor=lambda x:
-                                            insensitive_hist.sub(hist, x.upper())))
+                                        insensitive_hist.sub(hist, x.upper())))
             tracks.extend(format_tracks(hist, [y20o20_zinbra_peaks[i]],
                                         name_processor=lambda x: "ZINBRA " + x))
 
         tracks.extend(format_tracks(hist, encode_bws))
         tracks.extend(format_tracks(hist, encode_peaks,
                                     name_processor=lambda x:
-                                        ("MACS " if "broad" in x else "SICER ") + x))
+                                    ("MACS " if "broad" in x else "SICER ") + x))
         tracks.append(format_track(hist, LABELS_URL.format(hist)))
 
     with open(OUT_FOLDER + '/' + page, 'w') as file:
@@ -121,7 +86,7 @@ def format_track(hist, uri, name_processor=lambda x: x):
         style: {glyph: 'HISTOGRAM', BGCOLOR: 'rgb(@RGB@)', HEIGHT: 30, id: 'style1'}
     }],
     noDownsample: false
-}""".replace('@NAME@', name_processor(Path(uri).stem))\
+}""".replace('@NAME@', name_processor(Path(uri).stem)) \
         .replace('@URI@', uri).replace('@RGB@', get_color(hist, uri))
 
 
@@ -132,31 +97,45 @@ def generate_download_chipseq_page(page):
         template_html = file.read()
 
     def create_tr(hist):
-        return '<tr><th>{}</th>'.format(hist) + \
+        return '<tr>' + \
+               '<th>{}</th>'.format(hist) + \
                '<th><a href="{}">Reads</a></th>'.format(BEDGZ_PATH.format(hist)) + \
-               '<th><a href="{}">BigWigs</a></th>'.format(Y20O20_BW_PATH.format(hist)) + \
+               '<th><a href="{}">QC</a></th>'.format(FASTQC_PATH.format(hist)) + \
+               ('<th><a href="{}">BigWigs</a>&nbsp;' +
+                '<a href="{}.html" title="Explore data"><span class="glyphicon glyphicon-eye-open" '
+                'aria-hidden="true"></span></a></th>').format(Y20O20_BW_PATH.format(hist), hist) + \
                '<th><a href="{}">Peaks</a></th>'.format(PEAKS_PATH.format(hist, 'zinbra')) + \
                '<th><a href="{}">Labels</a></th>'.format(LABELS_URL.format(hist)) + \
-               '<th><a href="{}">Models</a></th>'.format(ZINBRA_MODELS_PATH) + '</tr>'
+               ('<th><a href="{}">Models</a>&nbsp;' +
+                '<a href="howto.html" title="Visual peak calling how to">' +
+                '<span class="glyphicon glyphicon-question-sign" ' +
+                'aria-hidden="true"></span></a></th>').format(ZINBRA_MODELS_PATH) + \
+               '</tr>'
 
-    table = """<table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Modification</th>
-                            <th>Reads</th>
-                            <th>Bigwigs</th>
-                            <th>Peaks</th>
-                            <th>Peak caller labels</th>
-                            <th>Peak caller models</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>""" + '\n'.join([create_tr(hist) for hist in GSM_HIST_MAP.keys()]) + \
-            """</tr>
-            </tbody>
-        </table>"""
     with open(OUT_FOLDER + '/' + page, 'w') as file:
-        file.write(template_html.replace('@TABLE@', table))
+        file.write(template_html.replace('@TABLE@',
+                                         '\n'.join([create_tr(hist) for hist in GSM_HIST_MAP.keys()])))
+
+
+def generate_download_public_data(page):
+    download_chipseq_template = FOLDER + '/_public_data.html'
+    print('Creating download data page {} by template {}'.format(page, download_chipseq_template))
+    with open(download_chipseq_template, 'r') as file:
+        template_html = file.read()
+
+    def create_tr(hist):
+        return '<tr>' + \
+               '<th>{}</th>'.format(hist) + \
+               ('<th><a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={0}">' +
+                '{0}</a></th>').format(GSM_HIST_MAP[hist]) + \
+               '<th><a href="{}/{}_hg19.bw">BigWigs</a></th>'.format(ENCODE_BW_PATH, GSM_HIST_MAP[hist]) + \
+               '<th><a href="{}">Peaks</a></th>'.format(ENCODE_PEAKS_PATH.format(hist)) + \
+               '<th><a href="{}">Labels</a></th>'.format(ENCODE_LABELS_URL.format(hist)) + \
+               '</tr>'
+
+    with open(OUT_FOLDER + '/' + page, 'w') as file:
+        file.write(template_html.replace('@TABLE@',
+                                         '\n'.join([create_tr(hist) for hist in GSM_HIST_MAP.keys()])))
 
 
 def generate_page(page, title, scripts, content):
@@ -187,10 +166,12 @@ def _cli():
     print('Generate static pages')
     generate_page('index.html', title='Multiomics dissection of healthy human aging',
                   scripts='', content='_index.html')
-    generate_page('paper.html',
-                  title='Paper', scripts='', content='_paper.html')
-    generate_page('software.html',
-                  title='Software', scripts='', content='_software.html')
+    generate_page('methods.html',
+                  title='Methods', scripts='', content='_methods.html')
+    generate_page('tools.html',
+                  title='Tools', scripts='', content='_tools.html')
+    generate_page('howto.html',
+                  title='Visual peak calling how to', scripts='', content='_howto.html')
 
     print('Creating explore data pages')
     for hist in GSM_HIST_MAP.keys():
@@ -209,6 +190,12 @@ def _cli():
     generate_download_chipseq_page(content_page)
     generate_page('download_chipseq.html',
                   title='Download ChIP-Seq', scripts='', content=content_page)
+
+    print('Creating download public data page')
+    content_page = '_public_data.html'
+    generate_download_public_data(content_page)
+    generate_page('public_data.html',
+                  title='Download Public Data', scripts='', content=content_page)
 
     print('Done')
 
